b7f1d0b5b774fdf4cbecbc198cd3a4c41341da48
LouisAntoine <louisantoine.pro@gmail.com>
2022-06-30 12:59:16 +0200

    Adding CO2 api + views

--- app/controllers/co2_controller.rb
+++ app/controllers/co2_controller.rb
@@ -1,3 +1,23 @@
 class CO2Controller < ApplicationController
-  def 
+  
+
+
+  def get_co2
+    const axios = require('axios');
+
+    axios.get('https://api.monimpacttransport.fr/beta/getEmissionsPerDistance', {
+    params: {
+      km: @distance
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    
+  });  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post "/distances/get_distance", to: "distances#get_distance", as: 'get_distance'  
+  post "/distances/get_distance", to: "distances#get_distance", as: 'get_distance' 
+  
+  get "/distances/get_co2", to: "co2#get_co2", as: 'get_co2' 
     
   root "distances#index" 
 end
